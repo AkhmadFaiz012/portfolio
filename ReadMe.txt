b3167eb8fa79eef8f69113641bb9cf03ecd2b3c4
AkhmadFaiz012 <akhmadf109@gmail.com>
2023-11-07 19:11:17 +0700

    first commit

---
+++ ReadMe.txt
@@ -0,0 +1,5 @@
+Created By:@hai_faiz12
+
+
+content yang akan ditampilkan/dituju TailwindCss
+./public/html/**/*.{html,js}
---
+++ tailwind.config.js
@@ -0,0 +1,34 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['landing.html'],
+  theme: {
+    extend: {
+      colors: {
+        "primary": '#a21caf',
+        "secondary": '#701a75',
+        "click-btn": '#ef4444',
+        "white": '#f7f7f7',
+        "gray": '#374151',
+        "dark": '#0f172a',
+        "dark-2": '#6b7280',
+        "dark-3": '#94a3b8',
+      },
+      fontFamily: {
+        Poppins: ["poppins, sans-serif"],
+        Montserrat: ["montserrat, sans-serif"],
+        Roboto: ["roboto, sans-serif"],
+      },
+      container: {
+        center: true,
+        padding: "1rem",
+        screens : {
+          lg: "1124px",
+          xl: "1124px",
+          "2xl": "1124px",
+        },
+      },
+    },
+  },
+  plugins: [],
+}
+
